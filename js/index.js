a3937ebb2f2e3852cb83d5489a05404a8f61dce0
Sreehas Dommata <sreehasdommata98@gmail.com>
2018-07-07 13:14:17 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 
-$(document).ready(function () {
-    $('.code-box-copy').codeBoxCopy();
-});
-
 function scrollNav() {
   $('.nav a').click(function(){  
     //Toggle Class
@@ -122,18 +118,69 @@ jQuery(function ($) {
 });
 
 
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
+  var that = this;
+  var delta = 300 - Math.random() * 100;
 
+  if (this.isDeleting) { delta /= 2; }
 
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
 
-$(document).ready(function () {
-    $('.code-box-copy').codeBoxCopy({
-        tooltipText: 'Copied',
-        tooltipShowTime: 1000,
-        tooltipFadeInTime: 300,
-        tooltipFadeOutTime: 300
-    });
-});
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+}
